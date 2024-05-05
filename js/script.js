72d0d5e645b405627120c5a0725cc59f9782d901
owokomoses <owokomose@gmail.com>
2024-05-05 21:20:28 +0300

    Added typing animation and smooth navigation

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["Web Developer", "Web Designer", "Javascript Developer", "Software Engineer"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
+
+//smooth scrolling
+document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const offsetTop = targetElement.offsetTop;
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
